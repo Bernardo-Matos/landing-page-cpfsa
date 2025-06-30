7c0191fa49d70cb91fb9880128c249243b258fd8
mariosaantana <mario.santana@qh4.com.br>
2025-06-30 16:38:53 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,67 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CPFSA Advogados - Indenizações por Acidentes",
-  description: "Especialistas em indenizações por acidentes em corridas de aplicativo",
+  description:
+    "Especialistas em indenizações por acidentes em corridas de aplicativo",
   generator: "v0.dev",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1641039326607802');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MTJXFS2X');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
+        {/* GTM noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MTJXFS2X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
 
-        {/* Ícone flutuante no canto inferior direito */}
+        {/* Ícone flutuante do WhatsApp */}
         <a
           href="https://wa.me/553191461571?text=Olá!%20Vi%20o%20anúncio%20no%20site%20e%20preciso%20de%20um%20especialista%20que%20me%20ajude%20num%20acidente%20que%20tive"
           target="_blank"
@@ -36,5 +76,5 @@ export default function RootLayout({
         </a>
       </body>
     </html>
-  )
+  );
 }
